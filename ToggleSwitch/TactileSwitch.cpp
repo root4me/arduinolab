7e53ff5cde8b38a6a8406b26e822515788bc7740
root4me <pixelstripes@gmail.com>
2014-04-20 15:13:28 -0500

    Initial commit

---
+++ ToggleSwitch/TactileSwitch.cpp
@@ -0,0 +1,44 @@
+// 
+// 
+// 
+
+#include "TactileSwitch.h"
+#include <Arduino.h>
+
+int _pin = -1;
+bool _enabled = false;
+unsigned long _previousVal = LOW;
+
+
+void TactileSwitch::init(int pin)
+{
+	pinMode(pin, INPUT);
+	_pin = pin;
+	
+	Serial.write("Pin Set..");
+
+}
+
+bool TactileSwitch::on()
+{
+	Serial.write("test..");
+
+	if (_pin == -1) return false;
+
+	unsigned long currentVal = digitalRead(_pin);
+	
+	if (currentVal == HIGH) 
+	{
+		if(_previousVal != currentVal)
+		{
+			_enabled = !(_enabled);
+		}
+	}
+
+	_previousVal = currentVal;
+	return _enabled;
+
+}
+
+
+
